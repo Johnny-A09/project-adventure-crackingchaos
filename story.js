fefaffc7dd26f7feb60f16439f30783e0ad2fab4
Johnny-A09 <alvarado.johnny159@gmail.com>
2026-04-24 10:41:05 -0700

    adds

---
+++ story.js
@@ -0,0 +1,9 @@
+export const story = {
+    start: {
+        intro: `In Los Angeles California, ${playerName} is an adult who is an illegal candy vendor along with friends. They do this because their family kicked them out of their home and is flat broke and needs the money in this new unfamiliar place. New to the scene, connections with ${connections}, so it’s harder on the uprise but ${playerName} starts with no heat. No money, no nothing, just guidance and ambition. Their hideout is based in an average suburban home where they illegally produce their candy. One day, the SGA (Sucrose and Glucose Administration) for their unlicensed candy production, conducts a raid on their hideout. ${playerName} escapes but their peers are caught and are now alone in the big city of Los Angeles.`,
+        choices: ,
+    }
+}
+
+
+
